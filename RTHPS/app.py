986d23fdba85eab71b5bc4780e62ee28f34e1df8
chronis10 <it21797@hua.gr>
2019-10-19 19:14:51 +0300

    read options.json parameters

--- RTHPS/app.py
+++ RTHPS/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +8,10 @@ def hello_world():
     return 'Flask Dockerized'
 
 if __name__ == '__main__':
+
+	with open('/data/options.json') as config_file:
+		data = json.load(config_file)
+	print(data)
+	print(data["mqtt_sensor_topic"])
     app.run(debug=True,host='0.0.0.0')
 	
