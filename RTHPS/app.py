59aa66d1dc15858ecf100234f38231fcf5a0de15
chronis10 <it21797@hua.gr>
2019-10-19 19:51:55 +0300

    fixes

--- RTHPS/app.py
+++ RTHPS/app.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+	return 'Flask Dockerized'
 
 if __name__ == '__main__':
-
 	with open('/data/options.json') as config_file:
 		data = json.load(config_file)
 	print(data)
 	print(data["mqtt_sensor_topic"])
     app.run(debug=True,host='0.0.0.0')
-	
