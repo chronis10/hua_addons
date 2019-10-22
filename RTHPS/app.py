7467e11bb665a05d264f7db9d6de15243c720756
Christos Sardianos <chsardianos@gmail.com>
2019-10-22 19:21:06 +0300

    Added library for pir-based recommendation trigger response.

--- RTHPS/app.py
+++ RTHPS/app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Flask Dockerized'
+        return 'Local Flask Dockerized instance'
 
 if __name__ == '__main__':
         with open('/data/options.json') as config_file:
                 data = json.load(config_file)
         print(data)
         print(data["mqtt_sensor_topic"])
+        print(data["db_username"])
         app.run(debug=True,host='0.0.0.0')
